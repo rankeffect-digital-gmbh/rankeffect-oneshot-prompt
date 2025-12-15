e675bb2bafec5bf16bae70198581f40bade70226
fabianreisner-rankeffect <fabian.reisner@rankeffect.de>
2025-12-15 11:09:48 +0100

    feat: add robots

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,14 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Rankeffect Media Gallery",
   description: "Vote on media content",
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
